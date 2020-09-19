446b91de92381791eaa232f97b2e9fac7cee2626
Philippe Antoine <contact@catenacyber.fr>
2020-09-19 11:14:38 +0200

    webfuzz: first commit

---
+++ libfuzzer/fuzzapi.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"reflect"
+	"unsafe"
+
+	"github.com/catenacyber/webfuzz/webfuzz"
+)
+
+// #cgo CFLAGS: -Wall -Werror
+// #ifdef __linux__
+// __attribute__((weak, section("__libfuzzer_extra_counters")))
+// #else
+// #error Currently only Linux is supported
+// #endif
+// unsigned char LibfuzzerExtraCounters[0x10000];
+import "C"
+
+//export LLVMFuzzerInitialize
+func LLVMFuzzerInitialize(argc uintptr, argv uintptr) int {
+	webfuzz.WebfuzzInitialize(unsafe.Pointer(&C.LibfuzzerExtraCounters[0]), 0x10000)
+	return 0
+}
+
+//export LLVMFuzzerTestOneInput
+func LLVMFuzzerTestOneInput(data uintptr, size uint64) int {
+	sh := &reflect.SliceHeader{
+		Data: data,
+		Len:  int(size),
+		Cap:  int(size),
+	}
+	input := *(*[]byte)(unsafe.Pointer(sh))
+	webfuzz.WebfuzzProcess(input)
+	// always return 0 as it is expected by libFuzzer
+	return 0
+}
---
+++ webfuzz/webfuzz.go
@@ -0,0 +1,127 @@
+package webfuzz
+
+//import "C"
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"unsafe"
+)
+
+const CoverSize = 0x10000
+
+var CoverTab = new([CoverSize]byte)
+
+//export WebfuzzInitialize
+func WebfuzzInitialize(coverTabPtr unsafe.Pointer, coverTabSize uint64) {
+	if coverTabSize != CoverSize {
+		panic("Incorrect cover tab size")
+	}
+	CoverTab = (*[CoverSize]byte)(coverTabPtr)
+}
+
+var host = "http://localhost:8065/"
+
+func unserializeRequest(input []byte) (*http.Request, error) {
+	if len(input) < 2 {
+		return nil, io.EOF
+	}
+	method := ""
+	offset := 1
+	switch input[0] {
+	case 0:
+		method = "GET"
+	case 1:
+		method = "POST"
+	case 2:
+		method = "PUT"
+	case 3:
+		method = "HEAD"
+	default:
+		offset = bytes.IndexByte(input[1:], ' ')
+		if offset < 0 {
+			return nil, io.EOF
+		}
+		method = string(input[1 : 1+offset])
+		offset += 2
+	}
+	urilen := bytes.IndexByte(input[offset:], '\n')
+	if urilen < 0 {
+		return nil, io.EOF
+	}
+	uri := string(input[offset : offset+urilen])
+	offset += urilen + 1
+	hnames := make([]string, 0, 8)
+	hvalues := make([]string, 0, 8)
+	for offset < len(input) {
+		//TODO allow LF and colon in headers
+		hlen := bytes.IndexByte(input[offset:], '\n')
+		if hlen < 0 {
+			return nil, io.EOF
+		} else if hlen == 0 {
+			break
+		}
+		header := string(input[offset : offset+hlen])
+		offset += hlen + 1
+		nv := strings.Split(header, ":")
+		if len(nv) != 2 {
+			return nil, io.EOF
+		}
+		hnames = append(hnames, nv[0])
+		hvalues = append(hvalues, nv[1])
+	}
+	req, err := http.NewRequest(method, host+uri, bytes.NewReader(input[offset:]))
+	if err != nil {
+		return req, err
+	}
+	for i := 0; i < len(hnames); i++ {
+		req.Header.Add(hnames[i], hvalues[i])
+	}
+	return req, err
+}
+
+//export WebfuzzProcess
+func WebfuzzProcess(input []byte) int {
+	req, err := unserializeRequest(input)
+	if err != nil {
+		return -1
+	}
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(fmt.Sprintf("Request failed : %s", err))
+	}
+
+	computeCoverage(req, resp)
+
+	resp.Body.Close()
+	return 0
+}
+
+var seenCodes = new([512]bool)
+
+func computeCoverage(req *http.Request, resp *http.Response) {
+	//512 counters for status code coverage
+	if resp.StatusCode >= 100 && resp.StatusCode < 611 {
+		CoverTab[resp.StatusCode-99]++
+		if !seenCodes[resp.StatusCode-99] {
+			seenCodes[resp.StatusCode-99] = true
+			fmt.Println("NEW webfuzz status code %d", resp.StatusCode)
+		}
+	} else {
+		CoverTab[0]++
+		panic(fmt.Sprintf("Unknown response code %d", resp.StatusCode))
+	}
+
+	//resp headers coverage
+
+	if resp.StatusCode < 399 {
+		//uri coverage
+		//resp body coverage
+	}
+}
+
+//func main() {}
